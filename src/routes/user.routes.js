ae55396be91856884ab6b32d8712c56bacd69c42
Lakshay <nijhawanlakshay06@gmail.com>
2024-02-16 22:23:51 +0530

    Login and Logout logic

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import {Router} from 'express';
-import {registerUser} from '../controllers/user.controller.js'
+import {registerUser,loginUser} from '../controllers/user.controller.js'
 //use multer to store images locally it is a middleware
 import {upload} from "../middlewares/multer.middleware.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 const router = Router();
 
@@ -19,5 +20,10 @@ router.route("/register").post(upload.fields([
         maxCount:1
     }
 ]),registerUser)
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT,logoutUser)
 
 export default router
